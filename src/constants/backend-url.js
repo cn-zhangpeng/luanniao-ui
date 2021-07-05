ddaed6fbe823f00f7b64f7cd065a77f24f5bdb0a
zhangpeng <zp95sky@163.com>
2021-07-05 14:44:06 +0800

    添加年维度按天软件使用的统计。

--- src/constants/backend-url.js
+++ src/constants/backend-url.js
@@ -4,6 +4,7 @@ export default {
   },
   software: {
     GET_SOFTWARE_LIST: '/software/list',
-    GET_SOFTWARE_WEEK_USE_TIME: '/software/time/weekStatistic'
+    GET_SOFTWARE_WEEK_USE_TIME: '/software/time/weekStatistic',
+    GET_SOFTWARE_YEAR_DATE_STATISTIC: '/software/time/yearDateStatistic'
   }
 };
--- src/views/software/UseTime.vue
+++ src/views/software/UseTime.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <div class="total-use">
-      <a-statistic title="总时间" :value="112893" style="margin-right: 50px" />
+      <a-statistic title="总时间（分钟）" :value="112893" style="margin-right: 50px" />
     </div>
-    <div id="week-use" style="width:600px;height:300px;">
-
-    </div>
+    <div id="week-use" style="width:600px;"></div>
+    <div id="year-calendar" style="width:1000px;"></div>
   </div>
 </template>
 
@@ -14,6 +13,21 @@ import { defineComponent, reactive, onMounted } from 'vue';
 import * as echarts from 'echarts';
 import Axios from '../../utils/http-util';
 import BackendUrl from '../../constants/backend-url';
+
+const getVirtualData = (year2) => {
+  let year = year2 || '2017';
+  let date = Number(echarts.number.parseDate(year + '-01-01'));
+  let end = Number(echarts.number.parseDate((Number(year) + 1) + '-01-01'));
+  let dayTime = 3600 * 24 * 1000;
+  let data = [];
+  for (let time = date; time < end; time += dayTime) {
+    data.push([
+      echarts.format.formatTime('yyyy-MM-dd', time),
+      Math.floor(Math.random() * 10000)
+    ]);
+  }
+  return data;
+};
 
 const state = reactive({
   weekUseData: {
@@ -33,6 +47,42 @@ const state = reactive({
     },
     yAxis: { type: 'value' },
     series: []
+  },
+  yearCalendarData: {
+    title: {
+      top: 30,
+      left: 'center',
+      text: '2021年软件使用时长'
+    },
+    tooltip: {
+      position: 'top',
+      formatter: function (p) {
+        return p.data[0] + ': ' + p.data[1];
+      }
+    },
+    visualMap: {
+      min: 0,
+      max: 1000,
+      type: 'piecewise',
+      orient: 'horizontal',
+      left: 'center',
+      top: 65
+    },
+    calendar: {
+      top: 120,
+      left: 30,
+      right: 30,
+      cellSize: ['auto', 13],
+      range: 2021,
+      itemStyle: {
+        borderWidth: 0.5
+      }
+    },
+    series: {
+      type: 'heatmap',
+      coordinateSystem: 'calendar',
+      data: []
+    }
   }
 });
 
@@ -56,11 +106,24 @@ const getWeekUseTime = () => {
   });
 };
 
+const getYearCalendar = () => {
+  Axios.get(BackendUrl.software.GET_SOFTWARE_YEAR_DATE_STATISTIC, null).then(res => {
+    console.log(res.data);
+    res.data.forEach(function(item) {
+      console.log(item);
+      state.yearCalendarData.series.data.push([item.curDate, item.useTimeLength]);
+
+      let yearCalendarChart = echarts.init(document.getElementById('year-calendar'));
+      yearCalendarChart.setOption(state.yearCalendarData);
+    });
+  });
+};
+
 export default defineComponent({
   setup() {
     onMounted(() => {
       getWeekUseTime();
-      console.log(state.weekUseData.legend.data);
+      getYearCalendar();
     });
 
     return {
